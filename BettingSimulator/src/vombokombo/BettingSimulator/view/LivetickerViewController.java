dc2f36cfde5babc2b1e855ac490e65681db90654
BigSeeProduction <luca.emanuel.weiss@gmail.com>
2015-03-04 12:11:15 +0100

    update

--- BettingSimulator/src/vombokombo/BettingSimulator/view/LivetickerViewController.java
+++ BettingSimulator/src/vombokombo/BettingSimulator/view/LivetickerViewController.java
@@ -6,13 +6,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class LivetickerViewController {
 
 	@FXML
-	private Label teamA;
+	private TextArea teamA;
 	@FXML
-	private Label teamB;
+	private TextArea teamB;
 	
 	@FXML
 	private Label time;
