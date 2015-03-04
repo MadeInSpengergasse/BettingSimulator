dfb66495cfff528be271d4cca472c57eedfb84ae
BigSeeProduction <luca.emanuel.weiss@gmail.com>
2015-03-04 17:28:04 +0100

    update

---
+++ BettingSimulator/src/vombokombo/BettingSimulator/view/RootLayoutController.java
@@ -0,0 +1,21 @@
+package vombokombo.BettingSimulator.view;
+
+import javafx.fxml.FXML;
+
+public class RootLayoutController {
+
+	public RootLayoutController() {
+
+	}
+
+	@FXML
+	private void initialize() {
+
+	}
+
+	@FXML
+	public void closeMenu() {
+		System.exit(0);
+	}
+
+}
