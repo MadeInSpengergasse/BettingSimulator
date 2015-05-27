3cc2aa45b5061ec8147f3d1756882371b96faebc
lukas <web16415@spengergasse.at>
2015-05-27 12:34:17 +0200

    update

--- src/vombokombo/BettingSimulator/controller/RootLayoutController.java
+++ src/vombokombo/BettingSimulator/controller/RootLayoutController.java
@@ -14,20 +14,33 @@ public class RootLayoutController {
 
     }
 
+    /**
+     * sets the mainapp
+     * @param mainapp is the mainapp you want to merge with your attribute mainapp
+     */
     public void setMainApp(MainApp mainapp) {
         this.mainapp = mainapp;
     }
 
+    /**
+     * things which should happen at the beginning
+     */
     @FXML
     private void initialize() {
 
     }
 
+    /**
+     * Closes the program
+     */
     @FXML
     public void closeMenu() {
         System.exit(0);
     }
 
+    /**
+     * The about Menu
+     */
     @FXML
     public void aboutMenu() {
         Alert alert = new Alert(AlertType.INFORMATION);
@@ -39,21 +52,33 @@ public class RootLayoutController {
     }
 
 
+    /**
+     * Method to create a new file
+     */
     @FXML
     private void newMenu() {
         PropertiesHelper.newM();
     }
 
+    /**
+     * Method to open a file
+     */
     @FXML
     private void openMenu() {
         PropertiesHelper.open();
     }
 
+    /**
+     * Method to save your file
+     */
     @FXML
     private void saveMenu() {
         PropertiesHelper.save();
     }
 
+    /**
+     * Method to save your file as something specific
+     */
     @FXML
     private void saveAsMenu() {
         PropertiesHelper.saveAs();
